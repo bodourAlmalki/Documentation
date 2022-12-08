e8a303e9271112d17a3afde3feb380e540540d2a
Bodour Almalki <bodouralmalkisy@gmail.com>
2022-12-08 11:07:31 +0200

    java script6

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+document.getElementById("validate").onclick = function(){
+    var first = document.getElementById("first_number").value;
+    var second = document.getElementById("second_number").value;
+    var multiply = first * second ;
+    alert ('Result =' + ' ' + multiply)
+}

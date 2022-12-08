b647e9f214874d7aa3e63127c1d43bfc0cf7d888
Bodour Almalki <bodouralmalkisy@gmail.com>
2022-12-08 16:18:22 +0200

    step7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,14 @@
+document.getElementById("validate").onclick= function(){
+    var shoe = document.getElementById("shoe_size").value;
+    var birth = document.getElementById("year").value;
+    
+    var first= shoe*2;
+    var second= first+5;
+    var third= second*50;
+    var fourth= third-birth;
+    var fifth= fourth+1766;
+    alert (fifth);
+
+
+
+}
---
+++ javascript-basics-02/step 1/step1.js
@@ -0,0 +1,7 @@
+//let images = document.querySelectorAll("img[src='image1_2.jpg']");
+let images = document.querySelectorAll(".images");
+
+for (let i = 0; i < images.length; i++) {
+	let image = images[i];
+	alert(image.getAttribute(image1.jpg));
+}

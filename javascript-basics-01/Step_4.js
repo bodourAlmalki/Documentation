2bab3ff66c1490a2c9089749083c39f2371f5cf7
Bodour Almalki <bodouralmalkisy@gmail.com>
2022-12-08 10:50:27 +0200

    java script4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,7 @@
+document.getElementById("validate").onclick= function(){
+    var name1= document.getElementById("name").value;
+    var  surName= document.getElementById("surname").value;
+    var city= document.getElementById("city").value;
+
+    alert('helo'+' ' + name1 + ' '+ surName +' from ' + city) 
+}

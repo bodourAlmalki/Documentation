0c4270156ee83809b491b46ab63379feefcca0f0
Bodour Almalki <bodouralmalkisy@gmail.com>
2022-12-09 12:41:55 +0200

    basic-03

---
+++ javascript-basics-03/step 3/step3_3.js
@@ -0,0 +1,12 @@
+function colorGreen() {
+    document.getElementById("text").style.color = "green";
+  }
+  
+  function colorRed() {
+    document.getElementById("text").style.color = "red";
+  }
+  
+  function colorBlue() {
+    document.getElementById("text").style.color = "blue";
+  }
+  
